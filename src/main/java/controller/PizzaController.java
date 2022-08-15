8f28043a81f08a0481a5275aff4747c7fb95bc7b
GlebGus <gusinec.lion.com@tut.by>
2022-08-15 19:35:36 +0300

    add test and he worked

--- src/main/java/controller/PizzaController.java
+++ src/main/java/controller/PizzaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PizzaController {
     @GetMapping("/")
-    public static String Home(){
+    public static String home(){
         return "home";
     }
 }
