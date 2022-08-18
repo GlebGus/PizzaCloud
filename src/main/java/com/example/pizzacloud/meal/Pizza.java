0d6b7e21450070825809fdfa97cc623797fa3700
GlebGus <gusinec.lion.com@tut.by>
2022-08-18 21:11:57 +0300

    add spring validation and change pizza class

--- src/main/java/com/example/pizzacloud/meal/Pizza.java
+++ src/main/java/com/example/pizzacloud/meal/Pizza.java
@@ -2,9 +2,15 @@ package com.example.pizzacloud.meal;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 @Data
 public class Pizza {
+@NotNull
+@Size(min = 2, message = "Name must be at least 2 characters long")
     private String name;
+@NotNull
+@Size (min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
