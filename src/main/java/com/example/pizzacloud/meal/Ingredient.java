445cc2beb2b0a648ec4f95a9c517ee6be86cddd6
GlebGus <gusinec.lion.com@tut.by>
2022-10-09 12:31:08 +0300

    connection to cassandra through docker and little change ingredient and pizza class

--- src/main/java/com/example/pizzacloud/meal/Ingredient.java
+++ src/main/java/com/example/pizzacloud/meal/Ingredient.java
@@ -1,13 +1,19 @@
 package com.example.pizzacloud.meal;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE,force = true)
+@Table("ingridients")
 public class Ingredient {
-    @Id
+    @PrimaryKey
     public final String id;
     public final String name;
     public final Type type;
--- src/main/java/com/example/pizzacloud/meal/Pizza.java
+++ src/main/java/com/example/pizzacloud/meal/Pizza.java
@@ -1,25 +1,34 @@
 package com.example.pizzacloud.meal;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.relational.core.mapping.Table;
-
-import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
 @Data
-@Table
+@Table("pizza")
 public class Pizza {
-    @Id
-    private Long id;
-    @Column(name="CREATEDAT")
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    private UUID id = Uuids.timeBased();
+    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private Date createdAt = new Date();
 @NotNull
 @Size(min = 4, message = "Name must be at least 4 characters long")
     private String name;
 @NotNull
 @Size (min = 1, message = "You must choose at least 1 ingredient")
+@Column("ingredients")
     private List<Ingredient> ingredients;
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
+
 }
